5e6e78ac9d6b19748063f44bb292193e1bfcfec7
Faisal Momoniat <97913874+faisalm1997@users.noreply.github.com>
2022-03-25 19:51:47 +0000

    Add files via upload

---
+++ Creating_databases_tables-2.sql
@@ -0,0 +1,24 @@
+#The general command for creating a database:
+
+CREATE DATABASE database_name; 
+
+#To drop a database:
+
+DROP DATABASE database_name; 
+
+#To use a database: 
+
+USE <database name>;
+
+#Showing table has been created: 
+
+SHOW TABLES;
+ 
+SHOW COLUMNS FROM tablename;
+ 
+DESC tablename;
+
+#Drop table: 
+
+DROP TABLE <tablename>;
+
